49634082dd66ae7f62acd399c3942d4334c27a58
marzieh moghtaderi <marzzy.m@gmail.com>
2021-06-10 17:39:16 +0430

    chore: add absolute path & add bundle size analysor

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from 'games/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
